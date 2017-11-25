08c45c695abfdd5db86316b241978d45d4af022e
Nuno Arruda <nuno@nunoarruda.com>
2017-11-25 18:42:23 -0100

    refactor: update logic to firestore

--- twitter.js
+++ twitter.js
@@ -1,21 +1,19 @@
-var Twitter = require('twitter');
-var firebase = require('firebase');
- 
-var client = new Twitter({
+const Twitter = require('twitter');
+const admin = require('firebase-admin');
+const serviceAccount = require('./twitter-jobs-firebase-adminsdk-es2pt-19d4de42f6.json');
+
+const client = new Twitter({
     consumer_key: 'EfeqfcYZ0hH5YsWPar8LCYJ1f',
     consumer_secret: 'O5IhScH1XRd3ecqaSVcdM76ugsjkJl3UOrLCsin60tgnbIyVjm',
     access_token_key: '258542343-Y4Mg64VKXgDBfkfHVfhPom3am0AGmMupi26v5dUa',
     access_token_secret: 'CkICReeLh3ocUnOnFWTJr3FAQZdSPMYkVsHceBGlFqGDn'
 });
 
-// Initialize the app with no authentication
-firebase.initializeApp({
-    databaseURL: 'https://twitter-jobs.firebaseio.com'
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
 });
 
-// The app only has access to public data as defined in the Security Rules
-var db = firebase.database();
-var ref = db.ref('/');
+const db = admin.firestore();
 
 var query_part1 = /\b(theme|html|javascript|css|ui|hybrid|js|html5|jquery|interactive|interface|fed|react|interaction|bootstrap|angular|ux|angularjs|ionic|cordova|front end|redux|front-end|css3|sass|web application|user experience|frontend|web app|typescript|web applications|phonegap|reactjs|web content|mobile web|angular.js|web ui|client-side|client side|html 5|react.js|css)\b/i;
 var query_part2 = /\b(development|engineering|developers|artist|developer|artists|expert|ninja|engineer|experts|hacker|guru|architects|engineers|specialist|consultants|consultant|dev|architect|samurai|wizard|specialists|artisan|producer|programmer|rockstar|hackers|wizards|artisans|ninjas|coder|programmers|gurus|devs|coders|rockstars|samurais)\b/i;
@@ -49,11 +47,9 @@ var save_to_db = function(data) {
         screen_name: data.user.screen_name
     };
 
-    ref.push(obj, function(error) {
-        if (error) {
-            console.log('Data could not be saved.' + error);
-        } else {
-            console.log('Data saved successfully.');
-        }
+    db.collection('tweets').add(obj).then(() => {
+        console.log('Data saved successfully.');
+    }).catch(error => {
+        console.log('Data could not be saved.' + error);
     });
 };
